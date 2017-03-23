0f9efab705a6ad0e1d31d0bce56d0fe3fc6d89f3
Maksim Samuilionak <maximwow70@gmail.com>
2017-03-23 20:55:55 +0300

    calories-app 0.0.8

--- src/javaProject/Calories/web/js/dish.js
+++ src/javaProject/Calories/web/js/dish.js
@@ -1,18 +1,84 @@
-function Dish(id, name, components){
+function ItemList(items){
+    this.itemList = items;
+    
+    this.dom = document.querySelector('.item_list');
+}
+ItemList.prototype.addChild = function(child){
+    this.dom.appendChild(child);
+}
+
+function Item(id, name, components){
     this.id = id;
     this.name = name;
     this.components = components;
-    this.addComponent = function(component){
-        this.components.push(component);
+
+    this.dom = document.createElement('div');
+    this.dom.setAttribute('class', 'item');
+
+    var itemNameHtml = document.createElement('div');
+    itemNameHtml.setAttribute('class', 'item-name');
+    itemNameHtml.textContent = name;
+    var itemComponentsHtml = document.createElement('div');
+    itemComponentsHtml.setAttribute('class', 'item-components');
+
+    if(components){
+        for (var i = 0; i < components.length; i++){
+            itemComponentsHtml.appendChild(
+                components[i].getDom()
+            );
+        }
     }
+
+    this.dom.appendChild(itemNameHtml);
+    this.dom.appendChild(itemComponentsHtml);
 }
+Item.prototype.addComponent = function(){
+    this.components.push(component);
+}
+Item.prototype.getDom = function(){
+    return this.dom;
+}
+
 function Component(id, name, calories, weight){
     this.id = id;
     this.name = name;
     this.calories = calories;
     this.weight = weight;
+
+    this.dom = document.createElement('div');
+    this.dom.setAttribute('class', 'component');
+
+    var componentIdHtml = document.createElement('span');
+    componentIdHtml.setAttribute('class', 'component-id');
+    componentIdHtml.textContent = id;
+
+    var componentNameHtml = document.createElement('span');
+    componentNameHtml.setAttribute('class', 'component-name');
+    componentNameHtml.textContent = name;
+
+    var componentDescriptionHtml = document.createElement('span');
+    componentDescriptionHtml.setAttribute('class', 'component-description');
+    componentDescriptionHtml.textContent = calories;
+
+    this.dom.appendChild(componentIdHtml);
+    this.dom.appendChild(componentNameHtml);
+    this.dom.appendChild(componentDescriptionHtml);
+}
+Component.prototype.getDom = function(){
+    return this.dom;
 }
 
-function DishHtml(){
-    this.dish = document.createElement('div');
-}
+
+//var i = new Item(1, 'apple pie', [{name: 'pie'}]);
+/* template using
+var components = [];
+components.push(new Component(1, 'pie', 100, 'qwe'));
+components.push(new Component(2, 'pie', '100', ''));
+components.push(new Component(3, 'pie', '100', ''));
+components.push(new Component(4, 'pie', '100', ''));
+
+var item = new Item('1', 'apple pie', components);
+
+var list = new ItemList();
+list.addChild(item.getDom());
+*/
--- src/javaProject/Calories/web/js/findDish.js
+++ src/javaProject/Calories/web/js/findDish.js
@@ -27,6 +27,7 @@ var toolbar = document.querySelector('.toolbar--find_dish');
 var control = document.querySelector('.toolbar-submit--find_dish');
 var dishList = document.querySelector('.item_list');
 
+ 
 function getSelectElements() {
     return document.querySelectorAll('.toolbar-select--components');
 }
@@ -49,25 +50,24 @@ function getDish(){
 }
 //setDishes([{name: 'apple pie', components: [{id: 1, name: 'pie'},{id: 2,name: 'apple'}]}]);
 function setDishes(arr){
+	var dishList = new ItemList();
 	var dishes = arr;
 	for (var i = 0; i < dishes.length; i++){
-		var dish = document.createElement('div');
-
-		var dishName = document.createElement('h2');
-		dishName.textContent = dishes[i].name;
-
-		var dishComponents = document.createElement('ul');
-		var dishComponentsList = dishes[i].components;
 		
+		var id = dishes[i].id;
+		var name = dishes[i].name;
+		
+		var dishComponentsList = dishes[i].components;
+		var components = [];
 		for (var j = 0; j < dishComponentsList.length; j++){
-			var component = document.createElement('li');
-			component.textContent += dishComponentsList[j].id + '.' + dishComponentsList[j].name;
-			dishComponents.appendChild(component);
+			var id = dishComponentsList[j].id;
+			var name = dishComponentsList[j].name;
+			var calories = dishComponentsList[j].calories;
+			var dishComponent = new Component(id, name, calories);
+			components.push(dishComponent);
 		}
-		dish.appendChild(dishName);
-		dish.appendChild(dishComponents);
-
-		dishList.appendChild(dish);
+		var dish = new Item(id, name, components);
+		dishList.addChild(dish.getDom());
 	}
 }
 function findDish(_dish){
