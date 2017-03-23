8f7964880dc9958ed9a7e4ff31d56bcb225fb17f
Maksim Samuilionak <maximwow70@gmail.com>
2017-03-23 17:29:10 +0300

    calories-app 0.0.5

--- src/javaProject/Calories/web/js/findDish.js
+++ src/javaProject/Calories/web/js/findDish.js
@@ -23,4 +23,66 @@ function newXMLHttpRequest() {
   return xmlreq;
 }
 
+/*
+function Dish(name, components){
+	this.name = name;
+	this.components = components;
+}
+function Component(){
+
+}
+*/
+
 var xhr = new newXMLHttpRequest();
+
+var toolbar = document.querySelector('.toolbar--find_dish');
+var control = document.querySelector('.toolbar-submit--find_dish');
+var dishList = document.querySelectorAll('.item_list');
+
+function getSelectElements() {
+    return document.querySelectorAll('.toolbar-select--components');
+}
+function getDishComponents(){
+	var selects = getSelectElements();
+	var components = [];
+	for (var i = 0; i < selects.length; i++){
+		components.push(selects[i].value);
+	}
+	return components;
+
+}
+function getDish(){
+	var dish = {};
+	var dishName = toolbar.querySelector('.toolbar-select--name').value;
+	var dishComponents = getDishComponents();
+	dish.dishName = dishName;
+	dish.names = dishComponents;
+	return dish;
+}
+function setDishes(arr){
+	var dishes = arr;
+	for (var i = 0; i < dishes.length; i++){
+		var dish = document.createElement('div');
+		dish.textContent = dishes[i].name;
+		dishList.appendChild(dish);
+	}
+}
+function findDish(_dish){
+	var dishes = [];
+	var dish = JSON.stringify(_dish);
+	var xhr = new newXMLHttpRequest();
+	xhr.open('POST', 'FindDish', true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(dish);
+	xhr.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+            dishes = JSON.parse(xhr.responseText);
+            setDishes(dishes);
+        }
+	}
+}
+
+control.addEventListener('click',function(){
+	var dish = getDish();
+	findDish(dish);
+});
