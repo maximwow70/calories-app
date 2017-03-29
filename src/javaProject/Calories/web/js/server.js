8c606ac804ce1f8cceda99b1b7e76174b8eea395
Maksim Samuilionak <maximwow70@gmail.com>
2017-03-29 15:58:34 +0300

    calories-app 3.0.0

---
+++ src/javaProject/Calories/web/js/server.js
@@ -0,0 +1,73 @@
+function Server(){
+}
+Server.prototype.getNewXhr = function(){
+    var xmlreq = false;
+    if (window.XMLHttpRequest) {
+    // Создадим XMLHttpRequest объект для не-Microsoft браузеров
+    xmlreq = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        // Создадим XMLHttpRequest с помощью MS ActiveX
+        try {
+            // Попробуем создать XMLHttpRequest для поздних версий
+            // Internet Explorer
+            xmlreq = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e1) {
+            // Не удалось создать требуемый ActiveXObject
+            try {
+            // Пробуем вариант, который поддержат более старые версии
+            //  Internet Explorer
+            xmlreq = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e2) {
+            // Не в состоянии создать XMLHttpRequest с помощью ActiveX
+            }
+        }
+    }
+    return xmlreq;
+}
+Server.prototype.addDish = function (itemList, _dish){
+    var dishes = [];
+	var dish = JSON.stringify(_dish);
+	var xhr = this.getNewXhr();
+	xhr.open('POST', 'AddDish', true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(dish);
+	xhr.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+            try {
+                dishes = JSON.parse(xhr.responseText);
+                itemList.setTitle('Your dish: ');
+                itemList.setItems(dishes);
+            }
+            catch(e) {
+                itemList.setTitle('Sorry, this dish is already exist. Try again!');
+            }
+        }
+	}
+}
+Server.prototype.findDish = function (itemList, _dish){
+	var dishes = [];
+	var dish = JSON.stringify(_dish);
+	var xhr = this.getNewXhr();
+	xhr.open('POST', 'FindDish', true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  	xhr.send(dish);
+	xhr.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+            dishes = JSON.parse(xhr.responseText);
+            itemList.setItems(dishes);
+        }
+	}
+} 
+Server.prototype.getComponents = function (toolbar){
+    var components = [];
+    var xhr = this.getNewXhr();
+    xhr.open('POST', 'BeginFindComponents', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+            components = JSON.parse(xhr.responseText);
+            toolbar.initComponents(components);
+        }
+    }
+}
