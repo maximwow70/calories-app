429bb2190320f6b42b0a851b49334b9947dc8661
Maksim Samuilionak <maximwow70@gmail.com>
2017-04-03 00:32:02 +0300

    calories-app 3.2.0

--- src/javaProject/Calories/web/js/item.js
+++ src/javaProject/Calories/web/js/item.js
@@ -5,56 +5,105 @@ function Item(id, name, components, weight, calories, src){
 	this.calories = calories;
     this.components = components;
 	this.src = src;
-	this.imgPath = 'url(img/' + this.src + ')';
-
+	this.imgPath = 'img/' + this.src + '';
+	
     this.dom = document.createElement('div');
-    
     this.dom.setAttribute('class', 'item');
 
-    var itemNameHtml = document.createElement('div');
-    itemNameHtml.setAttribute('class', 'item-name');
-    itemNameHtml.textContent = this.name;
-    this.dom.appendChild(itemNameHtml);
+	//itemFront
+	var itemFront = document.createElement('div');
+	itemFront.setAttribute('class', 'item-front');
 
-	var itemDescriptionHtml = document.createElement('div');
-    itemDescriptionHtml.setAttribute('class', 'item-description');
-	itemDescriptionHtml.style.backgroundImage = this.imgPath;
+	//itemName
+    var itemName = document.createElement('div');
+    itemName.setAttribute('class', 'item-name');
+    itemName.textContent = this.name;
+    itemFront.appendChild(itemName);
 
+	//itemImg
+	var itemImg = document.createElement('div');
+    itemImg.setAttribute('class', 'item-img');
+
+	var img = document.createElement('img');
+	img.setAttribute('src', this.imgPath);
+	img.setAttribute('class', 'img');
+	itemImg.appendChild(img);
+
+	itemFront.appendChild(itemImg);
+
+	//itemWeight
 	if (weight){
-		var itemWeightHtml = document.createElement('div');
-		itemWeightHtml.setAttribute('class', 'item-weight');
-		itemWeightHtml.innerHTML = 'Weight: ' + this.weight + 'g';
-		itemDescriptionHtml.appendChild(itemWeightHtml);
+		var itemWeight = document.createElement('div');
+		itemWeight.setAttribute('class', 'item-weight');
+		itemWeight.innerHTML = 'Weight: ' + this.weight + 'g';
+		itemFront.appendChild(itemWeight);
 	}
 
+	//itemCalories
 	if (calories){
 		var c = Math.round(this.calories * 100 / this.weight);
-		var itemCaloriesHtml = document.createElement('div');
-		itemCaloriesHtml.setAttribute('class', 'item-calories');
-		itemCaloriesHtml.innerHTML = 'Calories: ' + this.calories + ' (' + c + 'c/100g)';
-		itemDescriptionHtml.appendChild(itemCaloriesHtml);
+		var itemCalories = document.createElement('div');
+		itemCalories.setAttribute('class', 'item-calories');
+		itemCalories.innerHTML = 'Calories: ' + this.calories + ' (' + c + 'c/100g)';
+		itemFront.appendChild(itemCalories);
 	}
 
-    var itemComponentsHtml = document.createElement('div');
-    itemComponentsHtml.setAttribute('class', 'item-components');
+	//itemSelects
+	var itemSelectInfo = document.createElement('a');
+	itemSelectInfo.setAttribute('class', 'item-select item-select--info fa fa-info-circle');
+	itemFront.appendChild(itemSelectInfo);
+
+	var itemSelectAdd= document.createElement('a');
+	itemSelectAdd.setAttribute('class', 'item-select item-select--add fa fa-plus');
+	itemFront.appendChild(itemSelectAdd);
+
+	//itemBack
+	var itemBack = document.createElement('div');
+    itemBack.setAttribute('class', 'item-back');
+
+	//itemBackName
+    var itemComponentsName = document.createElement('div');
+    itemComponentsName.setAttribute('class', 'item-name');
+    itemComponentsName.textContent = 'components';
+    itemBack.appendChild(itemComponentsName);
+
+    var itemComponents = document.createElement('div');
+    itemComponents.setAttribute('class', 'item-components');
 
     if(components){
         for (var i = 0; i < components.length; i++){
-            itemComponentsHtml.appendChild(
+            itemComponents.appendChild(
                 components[i].getDom()
             );
         }
     }
 
-	var itemFilterHtml = document.createElement('div');
-    itemFilterHtml.setAttribute('class', 'filter');
+    itemBack.appendChild(itemComponents);
 
-	itemDescriptionHtml.appendChild(itemFilterHtml);
-    itemDescriptionHtml.appendChild(itemComponentsHtml);
+	var itemInfoClose = document.createElement('a');
+	itemInfoClose.setAttribute('class', 'item-select item-select--info fa fa-close');
+	itemBack.appendChild(itemInfoClose);
 
-	this.dom.appendChild(itemDescriptionHtml);
 
+	this.dom.appendChild(itemFront);
+	this.dom.appendChild(itemBack);
+
+	var that = this;
+	var selectInfo = this.dom.querySelectorAll('.item-select--info');
+	for (var i = 0; i < selectInfo.length; i++){
+		selectInfo[i].addEventListener('click', function(){
+			$(that.dom).toggleClass('item--click');
+		});
+		selectInfo[i].addEventListener('mouseover', function(){
+			$(that.dom).addClass('item--hover');
+		});
+		selectInfo[i].addEventListener('mouseleave', function(){
+			$(that.dom).removeClass('item--hover');
+		});
+	}
 	
+	
+
 
 }
 Item.prototype.addComponent = function(component){
